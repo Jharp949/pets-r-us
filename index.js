b26d45eb741be780b3f8fe85a60f22c0197f1e6f
James Harper <jharp949@gmail.com>
2023-06-18 17:15:46 -0700

    modified:   index.js 	modified:   views/index.ejs

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ app.use('/styles', express.static(__dirname + 'public/styles'))
 app.use('/images', express.static(__dirname + 'public/images'))
 app.use('/js', express.static(__dirname + 'public/js'))
 
+app.get('', (req, res) => {
+    res.sendFile(__dirname + '/views/index.ejs')
+})
+
 //Listen on port 300
 
 app.listen(port, () => console.info(`Listening on port ${port}`))

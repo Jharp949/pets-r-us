82745992671bac7ea4f19249f1facf1df5a21600
James Harper <jharp949@gmail.com>
2023-07-16 14:08:14 -0700

    Changes to be committed: 	modified:   index.js 	new file:   models/appointment.js 	new file:   public/data/services.json 	new file:   views/booking.ejs 	modified:   views/layouts/_footer.ejs 	modified:   views/layouts/_header.ejs

--- index.js
+++ index.js
@@ -3,12 +3,13 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
-
+const fs = require('fs');
+const path = require('path');
+const Customer = require('./models/customer');
+const Appointment = require('./models/appointment');
 
 const app = express();
 const port = 3000;
-
-const Customer = require('./models/customer'); // Import the Customer model
 
 const conn = 'mongodb+srv://web340-user1:wysKZJyxGUDvF8Nt@bellevueuniversity.rjtfcfy.mongodb.net/';
 
@@ -54,24 +55,31 @@ app.get('/register', (req, res) => {
     res.render('register');
   });
 
-  app.get('/customer-list', (req, res) => {
-    res.render('customers');
+app.get('/customer-list', (req, res) => {
+  res.render('customers');
+});
+  
+app.post('/register', (req, res) => {
+  const { customerId, email } = req.body;
+  
+  const newCustomer = new Customer({ customerId, email });
+  
+  newCustomer.save()
+    .then(() => {
+      res.redirect('/');
+    })
+    .catch(error => {
+        console.log(error);
+      res.status(500).send('Error registering customer.');
+    });
   });
-  
-  app.post('/register', (req, res) => {
-    const { customerId, email } = req.body;
-  
-    const newCustomer = new Customer({ customerId, email });
-  
-    newCustomer.save()
-      .then(() => {
-        res.redirect('/');
-      })
-      .catch(error => {
-          console.log(error);
-        res.status(500).send('Error registering customer.');
-      });
-  });
+
+app.get('/booking', (req, res) => {
+  // load services from the JSON file
+  const services = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'public/data/services.json'), 'utf8'));
+    
+  res.render('booking', { services });
+});
 
 //Listen on port 3000
 app.listen(port, () => console.info(`Listening on port ${port}`));
